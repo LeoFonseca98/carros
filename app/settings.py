1e286781d08833db15c0b4af536fff8705dda6ef
LeoFonseca98 <leo-fonseca@outlook.com>
2025-06-20 16:07:04 -0300

    atualização

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@20emdz1w8=95sqpq%z#&-u*jte=l5%%f5ggy2aus__hm#mos_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
